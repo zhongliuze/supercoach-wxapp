7a6627fcd4390f167f70b776664285032d7fd90c
liuzhongzhong <zhongliuze@gmail.com>
2019-12-16 17:30:28 +0800

    2019-12-16 17:30

--- pages/course/course/course.js
+++ pages/course/course/course.js
@@ -17,45 +17,92 @@ Page({
     endTimePickerArray: [], // 结束时间选择框列表
 
     startTimeShowArray: [], // 开始时间展示列表
-    endTimeShowArray: [],  // 结束时间展示列表
+    endTimeShowArray: [], // 结束时间展示列表
 
     startTimePickerIndex: [0, 0, 0], // 开始时间选中位置 
     endTimePickerIndex: [0, 0, 0], // 结束时间选中位置
-    
+
     remindType: 0, // 提醒类型：0不提醒，1开始时、30分钟前、1小时前……
     repeatType: 0, // 重复类型：0不重复，1每天、2每周、3每月
     repeatTimes: 0, // 重复次数, 实际次数为repeatTimes+1
 
     remindArray: ['无提醒', '开始时', '30分钟前', '1小时前', '3小时前', '6小时前', '1天前'], // 提醒类型字典
-    repeatArray: ['不重复', '每天', '每周', '每月'],  // 重复类型字典
+    remindResault: [0, 1, 30, 60, 180, 360, 86400],
+    repeatArray: ['不重复', '每天', '每周', '每月'], // 重复类型字典
+    repeatResault: [0, 1, 7, 30],
 
     fullDaySwitch: false, // 全天按钮打开/关闭
 
-    colorArray: [{ color: '#DC4F5A', selected: false },{ color: '#DC5960', selected: false },{ color: '#EA6E7F', selected: false },{ color: '#E89CA4', selected: true },{ color: '#306BF5', selected: false },{ color: '#5392E8', selected: false },{ color: '#F1A044', selected: false },{ color: '#F4BA40', selected: false },{ color: '#F1A044', selected: false },{ color: '#F4BA40', selected: false },{ color: '#439697', selected: false },{ color: '#58A45C', selected: false },{ color: '#6653A2', selected: false },{ color: '#07070B', selected: false },{ color: '#444756', selected: false },  // 色彩字典
+    colorArray: [{
+        color: '#DC4F5A',
+        selected: false
+      }, {
+        color: '#DC5960',
+        selected: false
+      }, {
+        color: '#EA6E7F',
+        selected: false
+      }, {
+        color: '#E89CA4',
+        selected: true
+      }, {
+        color: '#306BF5',
+        selected: false
+      }, {
+        color: '#5392E8',
+        selected: false
+      }, {
+        color: '#F1A044',
+        selected: false
+      }, {
+        color: '#F4BA40',
+        selected: false
+      }, {
+        color: '#F1A044',
+        selected: false
+      }, {
+        color: '#F4BA40',
+        selected: false
+      }, {
+        color: '#439697',
+        selected: false
+      }, {
+        color: '#58A45C',
+        selected: false
+      }, {
+        color: '#6653A2',
+        selected: false
+      }, {
+        color: '#07070B',
+        selected: false
+      }, {
+        color: '#444756',
+        selected: false
+      }, // 色彩字典
     ],
 
     selectColorIndex: 0, // 选中颜色下标
     scrollIntoView: '', // 颜色方块自动滚入位置
 
-    customTitle: '',  // 自定义标题内容
+    customTitle: '', // 自定义标题内容
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var selectDate = options.selectDate;  // 传递过来的选中时间
-    var timePickerArray = [];  // 时间选择框列表
-    var timeShowArray = [];  // 时间展示列表
+  onLoad: function(options) {
+    var selectDate = options.selectDate; // 传递过来的选中时间
+    var timePickerArray = []; // 时间选择框列表
+    var timeShowArray = []; // 时间展示列表
 
-    var timePickerDate = [];  // 时间日期选择框列表
+    var timePickerDate = []; // 时间日期选择框列表
     var timeShowDate = []; // 时间日期展示列表
 
-    var timePickerHour = [];  // 时间小时选择框列表
-    var timeShowHour = [];  // 时间小时展示列表
+    var timePickerHour = []; // 时间小时选择框列表
+    var timeShowHour = []; // 时间小时展示列表
 
-    var startTimePickerIndex = [0, 0, 0];  // 开始日期时间位置
-    var endTimePickerIndex = [0, 0, 0];  // 结束日期时间位置
+    var startTimePickerIndex = [0, 0, 0]; // 开始日期时间位置
+    var endTimePickerIndex = [0, 0, 0]; // 结束日期时间位置
 
     // 汉化moment的周时间展示
     moment.locale('zh-cn', {
@@ -66,7 +113,7 @@ Page({
     selectDate = selectDate.split("-");
 
     // 生成picker选择框日期数据
-    for (let i = 0; i < 60; i++) {
+    for (let i = 0; i < 5; i++) {
       timePickerDate.push(moment(selectDate[0] + '-' + selectDate[1] + '-' + selectDate[2]).add(i + 1, 'day').format('MM月DD日 ddd'));
       timePickerDate.unshift(moment(selectDate[0] + '-' + selectDate[1] + '-' + selectDate[2]).subtract(i, 'day').format('MM月DD日 ddd'));
 
@@ -120,6 +167,9 @@ Page({
       startTimePickerIndex[2] = endTimePickerIndex[2] = 1;
     }
 
+    console.log(timePickerArray);
+    console.log(timeShowArray);
+
     this.setData({
       fixedBottomButtonMargin: wx.getStorageSync('fixedBottomButtonMargin'), // 设置吸底按钮自适应高度
       startTimePickerArray: timePickerArray,
@@ -135,69 +185,78 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成 
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
   /**
    * 点击开始时间选择列表
    */
-  bindStartTimePickerChange: function (event) {
-    var startTimePickerIndex = event.detail.value;  // 开始时间选中位置
-    var endTimePickerIndex = [0, 0, 0];  // 结束时间选中位置
+  bindStartTimePickerChange: function(event) {
+    console.log(event.detail.value);
+    console.log('开始选中时间');
+    var startTimeShowArray = this.data.startTimeShowArray;
+    console.log(startTimeShowArray);
+    var startTimePickerIndex = event.detail.value; // 开始时间选中位置
+    var endTimePickerIndex = [0, 0, 0]; // 结束时间选中位置
 
-    var endTimePickerArray = this.data.endTimePickerArray; // 结束时间选择列表
-    var endTimeShowArray = this.data.endTimeShowArray;  // 结束时间展示列表
-    
+    var endTimePickerArray = []; // 结束时间选择列表
+    var endTimeShowArray = []; // 结束时间展示列表
+
     // 将结束日期列表中日期列表缩减至选中开始日期之后
     var timePickerDate = [];
     for (let i = 0; i < 60; i++) {
-      timePickerDate.push(moment(moment(this.data.startTimeShowArray[3][startTimePickerIndex[0]]).format('YYYY-MM-DD')).add(i, 'day').format('MM月DD日 ddd'));
+      timePickerDate.push(moment(moment(startTimeShowArray[3][startTimePickerIndex[0]]).format('YYYY-MM-DD')).add(i, 'day').format('MM月DD日 ddd'));
     }
     endTimePickerArray[0] = timePickerDate;
     endTimeShowArray[0] = timePickerDate;
+
+    console.log('结束选中时间');
+    console.log(startTimeShowArray);
+    console.log(startTimeShowArray[0][startTimePickerIndex[0]]);
+    console.log(startTimePickerIndex[0]);
 
     // 将结束时间列表中小时列表缩小至选中开始小时之后
     var timePickerHour = [];
@@ -211,11 +270,13 @@ Page({
         timeShowHour.push(i);
       }
     }
+    console.log(timePickerHour);
+    console.log(timeShowHour);
     endTimePickerArray[1] = timePickerHour;
     endTimeShowArray[1] = timeShowHour;
 
 
-    // 结束时间保持分钟与开始时间分钟数一致
+    // // 结束时间保持分钟与开始时间分钟数一致
     if (startTimePickerIndex[2] == 0) {
       // 选中了00分
       endTimePickerArray[2] = ['00分'];
@@ -225,9 +286,10 @@ Page({
       endTimePickerArray[2] = ['30分'];
       endTimeShowArray[2] = ['30'];
     }
+
     
     this.setData({
-      'startTimePickerIndex': startTimePickerIndex,
+      'startTimePickerIndex': event.detail.value,
       'endTimePickerIndex': endTimePickerIndex,
       'endTimePickerArray': endTimePickerArray,
       'endTimeShowArray': endTimeShowArray,
@@ -237,7 +299,7 @@ Page({
   /**
    * 点击结束时间选择列表
    */
-  bindEndTimePickerChange: function (event) {
+  bindEndTimePickerChange: function(event) {
     this.setData({
       'endTimePickerIndex': event.detail.value,
     })
@@ -246,7 +308,7 @@ Page({
   /**
    * 点击进入课程提醒选择页
    */
-  navigateToRemind: function () {
+  navigateToRemind: function() {
     wx.navigateTo({
       url: '../remind/remind?remindType=' + this.data.remindType,
     })
@@ -255,7 +317,7 @@ Page({
   /**
    * 点击进入课程重复选择页
    */
-  navigateToRepeat: function () {
+  navigateToRepeat: function() {
     wx.navigateTo({
       url: '../repeat/repeat?repeatType=' + this.data.repeatType + '&repeatTimes=' + this.data.repeatTimes,
     })
@@ -264,7 +326,7 @@ Page({
   /**
    * 整天Switch开关打开/关闭
    */
-  fullDaySwitch: function (event) {
+  fullDaySwitch: function(event) {
     console.log(event);
     this.setData({
       fullDaySwitch: event.detail.value,
@@ -274,7 +336,7 @@ Page({
   /**
    * 自定义颜色选择
    */
-  selectCustomColor: function (event) {
+  selectCustomColor: function(event) {
     var colorArray = this.data.colorArray;
     var colorIndex = event.currentTarget.dataset.index;
 
@@ -294,7 +356,7 @@ Page({
   /**
    * 点击进入自定义名称列表页
    */
-  navigateToCustomList: function (event) {
+  navigateToCustomList: function(event) {
     wx.navigateTo({
       url: '../custom/custom',
     })
@@ -312,7 +374,7 @@ Page({
   /**
    * 点击进入上课内容选择页
    */
-  navigateToLesson: function (event) {
+  navigateToLesson: function(event) {
     wx.navigateTo({
       url: '../lesson/lesson',
     })
@@ -321,45 +383,125 @@ Page({
   /**
    * 保存/创建任务数据
    */
-  bindSave: function (event) {
+  bindSave: function(event) {
     var _this = this;
     let timestamp = moment().valueOf();
-    $.post(
-      'task/plan', {
-        'coachid': wx.getStorageSync('coachid'),
-        'sign': util.getSign(timestamp), // 签名（coachid + token + timestamp 的 MD5值）
-        'timestamp': timestamp, //时间戳
-        'begin': '2019-12-15 09:00', // 开始时间（格式 yyyy-MM-dd HH:mm）
-        'end': '2019-12-15 10:00', // 结束时间（格式 yyyy-MM-dd HH:mm）
-        'coachStudentId': 0, // 教练与学员关系 ID
-        'courseContentId': 0, // 上课内容ID
-        'courseRecordId': 0, // 购课记录ID
-        'remind': 0, // 课前提醒（0：无提醒、1、30、60、180、360、86400
-        'repeatCycle': 0, // 重复周期
-      },
-      function (res) {
-        console.log(res.data);
-        if (res.data.code == 0) {
-          // 获取成功
-          wx.showToast({
-            title: '创建成功',
-            icon: 'success',
-            success: function () {
-              setTimeout(function () {
-                wx.navigateBack({
-                  delta: '1'
-                })
-              }, 1500);
-            }
 
-          })
-        } else {
-          wx.showToast({
-            title: '创建失败',
-            icon: 'none'
-          })
-        }
-      }
-    )
+
+
+    console.log(this.data.startTimeShowArray[0][this.data.startTimePickerIndex[0]]);
+    console.log(this.data.startTimeShowArray[1][this.data.startTimePickerIndex[1]]);
+    console.log(this.data.startTimeShowArray[2][this.data.startTimePickerIndex[2]]);
+
+    console.log(this.data.endTimeShowArray[0][this.data.endTimePickerIndex[0]]);
+    console.log(this.data.endTimeShowArray[1][this.data.endTimePickerIndex[1]]);
+    console.log(this.data.endTimeShowArray[2][this.data.endTimePickerIndex[2]]);
+
+    // console.log(this.data.startTimePickerIndex);
+    // console.log(this.data.endTimePickerIndex);
+
+    // console.log(this.data.startTimePickerArray[0][this.data.startTimePickerIndex[0]]);
+    // console.log(this.data.startTimePickerArray[1][this.data.startTimePickerIndex[1]]);
+    // console.log(this.data.startTimePickerArray[2][this.data.startTimePickerIndex[2]]);
+
+    // console.log(this.data.endTimePickerArray[0][this.data.endTimePickerIndex[0]]);
+    // console.log(this.data.endTimePickerArray[1][this.data.endTimePickerIndex[1]]);
+    // console.log(this.data.endTimePickerArray[2][this.data.endTimePickerIndex[2]]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    var startTimeShowArray = this.data.startTimeShowArray;
+    var startTimePickerIndex = this.data.startTimePickerIndex;
+
+    var endTimeShowArray = this.data.endTimeShowArray;
+    var endTimePickerIndex = this.data.endTimePickerIndex;
+
+
+
+    // console.log(this.data.startTimePickerArray);
+    // console.log(this.data.startTimeShowArray);
+
+    // console.log(this.data.endTimePickerArray);
+    // console.log(this.data.endTimeShowArray);
+
+
+    // console.log(this.data.startTimePickerIndex);
+    // console.log(this.data.endTimePickerIndex);
+
+
+    // 获取选中的开始时间
+    var beginTime = startTimeShowArray[3][startTimePickerIndex[0]] + ' ' + startTimeShowArray[1][startTimePickerIndex[1]] + ':' + startTimeShowArray[2][startTimePickerIndex[2]];
+    // 获取选中的结束时间
+    var endTime = endTimeShowArray[3][endTimePickerIndex[0]] + ' ' + endTimeShowArray[1][endTimePickerIndex[1]] + ':' + endTimeShowArray[2][endTimePickerIndex[2]];
+
+
+    var remindValue = this.data.remindResault[this.data.remindType];
+    var repeatValue = this.data.repeatResault[this.data.repeatType];
+
+    // console.log(remindValue);
+    // console.log(repeatValue);
+    // console.log(this.data.repeatTimes);
+    // console.log(beginTime);
+    // console.log(endTime);
+    // $.post(
+    //   'task/plan', {
+    //     'coachid': wx.getStorageSync('coachid'),
+    //     'sign': util.getSign(timestamp), // 签名（coachid + token + timestamp 的 MD5值）
+    //     'timestamp': timestamp, //时间戳
+    //     'begin': beginTime, // 开始时间（格式 yyyy-MM-dd HH:mm）
+    //     'end': endTime, // 结束时间（格式 yyyy-MM-dd HH:mm）
+    //     'coachStudentId': 1, // 教练与学员关系 ID
+    //     'remind': remindValue, // 课前提醒（0：无提醒、1、30、60、180、360、86400
+    //     'repeat': repeatValue, // 重复类型（0：不重复、1、7、30）
+    //     'repeatCycle': this.data.repeatTimes, // 重复周期
+    //     'courseRecordId': 0, // 购课记录ID（课程类型？）
+    //     'courseContentId': 0, // 上课内容ID
+    //   },
+    //   function (res) {
+    //     console.log(res.data);
+    //     if (res.data.code == 0) {
+    //       // 获取成功
+    //       wx.showToast({
+    //         title: '创建成功',
+    //         icon: 'success',
+    //         success: function () {
+    //           setTimeout(function () {
+    //             wx.navigateBack({
+    //               delta: '1'
+    //             })
+    //           }, 1500);
+    //         }
+    //       })
+    //     } else {
+    //       wx.showToast({
+    //         title: '创建失败',
+    //         icon: 'none'
+    //       })
+    //     }
+    //   }
+    // )
   }
 })
--- pages/students/add/add.js
+++ pages/students/add/add.js
@@ -1,4 +1,10 @@
 // pages/students/add/add.js
+const moment = require('../../../vendor/moment/moment.js');
+import $ from '../../../common/common.js';
+const util = require('../../../utils/util')
+const md5 = require('../../../vendor/md5/md5.min.js');
+
+
 Page({
 
   /**
@@ -7,6 +13,7 @@ Page({
   data: {
     inputMore: false,
     fixedBottomButtonMargin: 0, // 吸底按钮的自适应高度
+    name: '', // 输入姓名
   },
 
   /**
@@ -67,10 +74,81 @@ Page({
 
   },
 
+  /**
+   * 展开填写更多信息
+   */
   inputMoreInfo: function(event) {
     console.log(event);
     this.setData({
       inputMore: true,
     });
+  },
+
+  /**
+   * 输入姓名
+   */
+  inputName: function(event) {
+    this.setData({
+      'name': event.detail.value,
+    });
+  },
+
+  /**
+   * 新增学员
+   */
+  addStudent: function (event) {
+    console.log(event);
+    var _this = this;
+    let timestamp = moment().valueOf();
+    if (!event.detail.value.name) {
+      wx.showToast({
+        title: '请输入学员姓名',
+        icon: 'none',
+      })
+      return ;
+    }
+    if (event.detail.value.mobile && event.detail.value.mobile.length < 11) {
+      wx.showToast({
+        title: '手机号格式错误',
+        icon: 'none',
+      })
+      return;
+    }
+    // $.post(
+    //   'coachStudent', {
+    //     'coachid': wx.getStorageSync('coachid'),
+    //     'sign': util.getSign(timestamp), // 签名（coachid + token + timestamp 的 MD5值）
+    //     'timestamp': timestamp, // 时间戳
+    //     'name': '溜小狗', // 学员姓名
+    //     'mobile': '', // 学员手机号
+    //     'courseTypeId': '', // 课程类型
+    //     'courseNumber': '', // 总课时数
+    //     'sourceId': '', // 学员来源ID
+    //     'remark': '', // 备注信息
+    //   },
+    //   function (res) {
+    //     console.log(res.data);
+    //     if (res.data.code == 0) {
+    //       // 获取成功
+    //       wx.showToast({
+    //         title: '创建成功',
+    //         icon: 'success',
+    //         success: function () {
+    //           setTimeout(function () {
+    //             wx.navigateBack({
+    //               delta: '1'
+    //             })
+    //           }, 1500);
+    //         }
+
+    //       })
+    //     } else {
+    //       wx.showToast({
+    //         title: '创建失败',
+    //         icon: 'none'
+    //       })
+    //     }
+    //   }
+    // )
   }
 })
